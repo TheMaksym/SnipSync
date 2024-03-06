04f3ba8a96bd8a6f65df18e32302a6c8c1784a78
Connor McLoon <cmcloon@ufl.edu>
2024-03-06 15:06:34 -0500

    Functional frontend call to backend api

--- INSTALLATION GUIDE.md
+++ INSTALLATION GUIDE.md
@@ -11,3 +11,9 @@ npm -v (Should be 10.4.0)
 to run the app, cd to 
 ./SnipSync/snipsync (should be within directory to where you can access files)
 and run "npm run dev" in terminal
+
+ALTERNATIVE RUN GUIDE:
+ok so you still have to do npm run dev in terminal, but while that is running in terminal you can go to the run icon on the left
+and once you do that, you can click the dropdown for configuration FrontEnd/BackEnd
+After that you should be able to re-run and debug front and backend simultaneously as much as you like
+(I wrote launch instructions in .vscode/launch.json)
--- snipsync/src/app/page.js
+++ snipsync/src/app/page.js
@@ -1,10 +1,42 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <p1>Hello World</p1>
+        
+        <form>
+          <label>
+            <div>
+              Username:
+              <input type="text" name="username" />
+            </div>
+            <div>
+              Password:
+              <input type="text" name="password" />
+            </div>
+            <div>
+            Account Name:
+            <input type="text" name="accountName" />
+            </div>
+          </label>
+          <div>
+          <input type="button" value="Create Account" />
+          </div>
+          <div>
+          <input type="button" value="Sign in" onClick={() => App()} />
+          </div>
+        </form>
+        
+        <p>The value is {App()} </p>
+        
+
+        {/*
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
@@ -89,7 +121,26 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+  */}
       </div>
     </main>
   );
+}
+
+function App(){
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:5050/user/')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+    </div>
+  );
+
 }
