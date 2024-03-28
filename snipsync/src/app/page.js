bed1ea18e77b4f2a7464a5184426d1cb9cc352d1
Connor McLoon <cmcloon@ufl.edu>
2024-03-27 23:14:35 -0400

    SignInFunctionality

--- snipsync/src/app/page.js
+++ snipsync/src/app/page.js
@@ -5,6 +5,10 @@ import styles from "./page.module.css";
 import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+  
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -17,18 +21,70 @@ export default function Home() {
         <form className={styles.form}>
           <div className={styles.inputGroup}>
             <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" />
+            <input type="text" id="username" name="username" value={username} onChange={(event) => setUsername(event.target.value)}/>
           </div>
           <div className={styles.inputGroup}>
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
+            <input type="password" id="password" name="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
           </div>
           <div className={styles.buttons}>
-            <input type="submit" value="Sign In" />
-            <input type="button" value="Create Account" />
+            <input type="button" value="Sign In"  onClick={() => SignIn(username,password)}/>
+            <input type="button" value="Create Account" onClick={() => CreateAccount(username, password)} />
           </div>
         </form>
       </div>
     </main>
   );
+}
+
+async function CreateAccount(username, password){
+  const requestOptions ={
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      password
+    })
+  }
+  
+  const response = await fetch('http://localhost:5050/User/Create', requestOptions)
+  
+  if(response.status==403){
+    console.log("Username already detected");
+  }
+  else if(response.status==200){
+    console.log("Success!");
+  }
+  else{
+    console.log(response.statusText)
+  }
+
+}
+
+async function SignIn(username, password){
+  const requestOptions ={
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      password
+    })
+  }
+  const response = await fetch('http://localhost:5050/User/validate/', requestOptions)
+  
+  if(response.status==403){
+    console.log("DENIED");
+  }
+  else if(response.status==200){
+    console.log("Success!");
+  }
+  else{
+    console.log(response.statusText)
+  }
 }
