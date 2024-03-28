7bfd40f56d27a9157a126cf32cd6851660baac71
Adam Lim <alim4@ufl.edu>
2024-03-28 00:32:27 -0400

    Added Foundation for NavBar

---
+++ snipsync/src/app/Pages/Home.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+import React, { useState, useEffect } from 'react';
+
+
+export function Home() {
+    return(
+        <main className={styles.main}>
+      <div className={styles.description}>
+        <Image
+          src="/images/Logo.png" 
+          alt="Logo"
+          width={100} 
+          height={100}
+        />
+        <form className={styles.form}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <div className={styles.buttons}>
+            <input type="submit" value="Sign In" />
+            <input type="button" value="Create Account" />
+          </div>
+        </form>
+      </div>
+    </main>
+    )
+    
+}
--- snipsync/src/app/page.js
+++ snipsync/src/app/page.js
@@ -1,34 +1,16 @@
 "use client"
-
 import Image from "next/image";
 import styles from "./page.module.css";
 import React, { useState, useEffect } from 'react';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Page1 } from "./Pages/Page1";
+import { Page2 } from "./Pages/Page2";
+import { Lay } from "./Components/Lay";
 
-export default function Home() {
+
+export default function App() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <Image
-          src="/images/Logo.png" 
-          alt="Logo"
-          width={100} 
-          height={100}
-        />
-        <form className={styles.form}>
-          <div className={styles.inputGroup}>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" />
-          </div>
-          <div className={styles.inputGroup}>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
-          </div>
-          <div className={styles.buttons}>
-            <input type="submit" value="Sign In" />
-            <input type="button" value="Create Account" />
-          </div>
-        </form>
-      </div>
-    </main>
+    <Home/>
   );
 }
