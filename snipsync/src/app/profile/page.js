c63dbe08aafded1822bbce4240b890d5578df7b8
Adam Lim <alim4@ufl.edu>
2024-04-24 15:45:41 -0400

    Test username

--- snipsync/src/app/profile/page.js
+++ snipsync/src/app/profile/page.js
@@ -2,28 +2,41 @@
 
 import Navbar from '../../components/Navbar';
 import NavbarProfile from '@/components/NavbarProfile';
-import Home from '../page';
 import styles from './profile.module.css'
-import axios from 'axios';
 import Image from 'next/image'
 
+import React, { useEffect, useState} from 'react'
 
 
+//<Navbar/>
+            //<NavbarProfile/>
+            //<div className={styles.imageContainer}>
+              //<Image
+                //src="/images/SnipSyncLogo.png"
+                //alt="pfp"
+                //width={100}
+                //height={100}
+              ///>
+            //</div>
 
 
 export default function Profile(){
+    const [username, setuserName] = useState('');
+
+    useEffect(() => {
+      const storedName = localStorage.getItem("username");
+      if(storedName) {
+        setuserName(storedName);
+      }
+    }, []);
+  
+
+
+
     return(
         <div className={styles.Home}>
-            <Navbar/>
-            <NavbarProfile/>
-            <div className={styles.imageContainer}>
-              <Image
-                src="/images/SnipSyncLogo.png"
-                alt="pfp"
-                width={100}
-                height={100}
-              />
-            </div>
+            <h1>Stored Name: {username}</h1>
+            
                                       
         </div>
             
