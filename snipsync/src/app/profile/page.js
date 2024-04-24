33b550a35ff441e3b35c461e23df9a4f238b1b3c
Maksym Marek <maksym.marek@ufl.edu>
2024-04-24 16:05:40 -0400

    Drop down menu for dashboard

--- snipsync/src/app/profile/page.js
+++ snipsync/src/app/profile/page.js
@@ -1,7 +1,70 @@
-'use client'
+"use client";
 
-export default function SignUp(){
-    return(
-    <p>profile</p>
-    )
+import styles from "./profile.module.css";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function profile() {
+  const router = useRouter();
+  const [twitchToken, setTwitchToken] = useState("");
+  const [youtubeToken, setYotubeToken] = useState("");
+
+  useEffect(() => {
+    setTwitchToken(localStorage.getItem("access_token_twitch"));
+    setYotubeToken(localStorage.getItem("access_token"));
+  }, []);
+
+  function logout() {
+    localStorage.setItem("authenticated", "false");
+    localStorage.setItem("username", "");
+    localStorage.setItem("access_token", "");
+    localStorage.setItem("access_token_twitch", "");
+    router.push("/");
+  }
+
+  function UnlinkTwitch(){
+    localStorage.setItem("access_token_twitch", "");
+    router.push("/dashboard");
+  }
+
+  function UnlinkYoutube(){
+    localStorage.setItem("access_token", "");
+    router.push("/dashboard");
+  }
+
+  return (
+    <>
+      <div className={styles.main}>
+        <p>WIP Profile Page</p>
+        <button className={styles.login} onClick={() => logout()}>
+          Logout
+        </button>
+        {twitchToken === "" ? (
+          <a
+            className={styles.login}
+            href="http://localhost:5050/twitch/user/signin"
+          >
+            Link Twitch
+          </a>
+        ) : (
+          <button className={styles.login} onClick={() => UnlinkTwitch()}>
+          Unlink Twitch
+        </button>
+        )}
+
+        {youtubeToken === "" ? (
+        <a
+          className={styles.login}
+          href="http://localhost:5050/youtube/user/signin"
+        >
+          Link Youtube
+        </a>
+        ) : (
+          <button className={styles.login} onClick={() => UnlinkYoutube()}>
+          Unlink Youtube
+        </button>
+        )}
+      </div>
+    </>
+  );
 }
